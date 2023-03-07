bc9a4a436c814b4589d601ba396e9b8bf41b0dc9
olganow <olganow@yandex.ru>
2023-03-07 17:03:05 +0300

    fix: EpicTaskTest

--- src/test/EpicTaskTest.java
+++ src/test/EpicTaskTest.java
@@ -10,63 +10,67 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EpicTaskTest {
-
     TaskManager taskManager = Managers.getDefault();
-
-    private EpicTask epicTask;
+    private EpicTask epicTaskOne;
+    private EpicTask epicTaskSecond;
+    private static final String VALIDATION_MESSAGE_FOR_EPIC_LIST = "The epic's subtask list is not empty";
     private static final String VALIDATION_MESSAGE_FOR_EPIC_STATUS = "The status of the Epic isn't correct";
+    private Subtask subtaskOne;
+    private Subtask subtaskSecond;
 
     @BeforeEach
-    private void beforeEach()  {
-        epicTask = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTask);
+    private void beforeEach() {
+        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
+        taskManager.addEpicTask(epicTaskOne);
+        epicTaskSecond = new EpicTask("First epic for testing", "Desc FE for testing ");
+        taskManager.addEpicTask(epicTaskSecond);
+
+        subtaskOne = new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW, 1);
+        taskManager.addNewSubTask(subtaskOne);
+        subtaskSecond = new Subtask("Second subtask for testing", "Desc SSB", TaskStatus.NEW, 1);
+        taskManager.addNewSubTask(subtaskSecond);
     }
 
     @Test
     @DisplayName("Пустой список подзадач")
     public void shouldBeEmptyListOfSubtasks() {
-        assertTrue(epicTask.getSubtaskIds().isEmpty(),"The epic's subtask list is not empty ");
+        assertTrue(epicTaskOne.getSubtaskIds().isEmpty(), VALIDATION_MESSAGE_FOR_EPIC_LIST);
     }
-
 
     @Test
     @DisplayName("Все подзадачи со статусом NEW")
     public void shouldHasStatusNewWhenAllSubtasksHaveStatusNew() {
-        Subtask subtask1 = new Subtask("First subtask", "Desc FSB", TaskStatus.NEW, 0);
-        taskManager.addNewSubTask(subtask1);
-        Subtask subtask2 = new Subtask("Second subtask", "Desc SSB", TaskStatus.NEW, 0);
-        taskManager.addNewSubTask(subtask2);
-        assertEquals(TaskStatus.NEW, epicTask.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
+        subtaskOne.setTaskStatus(TaskStatus.NEW);
+        subtaskSecond.setTaskStatus(TaskStatus.NEW);
+        taskManager.updateEpicTask(epicTaskSecond);
+        assertEquals(TaskStatus.NEW, epicTaskSecond.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
     }
 
     @Test
     @DisplayName("Все подзадачи со статусом DONE")
     public void shouldHasStatusDoneWhenAllSubtasksHaveStatusDone() {
-        Subtask subtask1 = new Subtask("First subtask", "Desc FSB", TaskStatus.DONE, 0);
-        taskManager.addNewSubTask(subtask1);
-        Subtask subtask2 = new Subtask("Second subtask", "Desc SSB", TaskStatus.DONE, 0);
-        taskManager.addNewSubTask(subtask2);
-        assertEquals(TaskStatus.DONE, epicTask.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
+        subtaskOne.setTaskStatus(TaskStatus.DONE);
+        subtaskSecond.setTaskStatus(TaskStatus.DONE);
+        taskManager.updateEpicTask(epicTaskSecond);
+        assertEquals(TaskStatus.DONE, epicTaskSecond.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
     }
 
     @Test
     @DisplayName("Подзадачи со статусами NEW и DONE")
     public void shouldHasStatusInProgressWhenAllSubtasksHaveStatusNewAndDone() {
-        Subtask subtask1 = new Subtask("First subtask", "Desc FSB", TaskStatus.NEW, 0);
-        taskManager.addNewSubTask(subtask1);
-        Subtask subtask2 = new Subtask("Second subtask", "Desc SSB", TaskStatus.DONE, 0);
-        taskManager.addNewSubTask(subtask2);
-        assertEquals(TaskStatus.IN_PROGRESS, epicTask.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
+        subtaskOne.setTaskStatus(TaskStatus.NEW);
+        subtaskSecond.setTaskStatus(TaskStatus.DONE);
+        taskManager.updateEpicTask(epicTaskSecond);
+        assertEquals(TaskStatus.IN_PROGRESS, epicTaskSecond.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
     }
 
     @Test
     @DisplayName("Подзадачи со статусами IN_PROGRESS")
     public void shouldHasStatusInProgressWhenAllSubtasksHaveStatusInProgress() {
-        Subtask subtask1 = new Subtask("First subtask", "Desc FSB", TaskStatus.IN_PROGRESS, 0);
-        taskManager.addNewSubTask(subtask1);
-        Subtask subtask2 = new Subtask("Second subtask", "Desc SSB", TaskStatus.IN_PROGRESS, 0);
-        taskManager.addNewSubTask(subtask2);
-        assertEquals(TaskStatus.IN_PROGRESS, epicTask.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
+        subtaskOne.setTaskStatus(TaskStatus.IN_PROGRESS);
+        subtaskSecond.setTaskStatus(TaskStatus.IN_PROGRESS);
+        taskManager.updateEpicTask(epicTaskSecond);
+        assertEquals(TaskStatus.IN_PROGRESS, epicTaskSecond.getTaskStatus(), VALIDATION_MESSAGE_FOR_EPIC_STATUS);
     }
 
 }
