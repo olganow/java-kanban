70359937d09df5245ed391198f216e541d046e12
olganow <olganow@yandex.ru>
2023-03-13 18:45:17 +0300

    feat: added test in InMemoryTaskManagerTest

--- src/test/main/java/manager/TaskManagerTest.java
+++ src/test/main/java/manager/TaskManagerTest.java
@@ -1,4 +1,5 @@
 package main.java.manager;
+
 import main.java.description.TaskStatus;
 import main.java.task.EpicTask;
 import main.java.task.SingleTask;
@@ -11,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-abstract class TaskManagerTest <T extends TaskManager> {
+abstract class TaskManagerTest<T extends TaskManager> {
 
     protected T taskManager;
 
@@ -19,15 +20,15 @@ abstract class TaskManagerTest <T extends TaskManager> {
 
         return new EpicTask("First epic for testing", "Desc FE for testing ");
     }
+
     protected Subtask createSubtask(EpicTask epic) {
         return new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW,
                 Instant.ofEpochMilli(163857900000L), 707568400L, epic.getId());
     }
 
 
-
     protected SingleTask createSingleTask() {
-        return new SingleTask( "First Single Task for testing", "Desc SST", TaskStatus.NEW,
+        return new SingleTask("First Single Task for testing", "Desc SST", TaskStatus.NEW,
                 Instant.ofEpochMilli(163857900000L), 57900000L);
     }
 
@@ -218,7 +219,7 @@ abstract class TaskManagerTest <T extends TaskManager> {
 
     @Test
     @DisplayName("Удаление с некорректным ID")
-    public void shouldValidateDeleteWhenDeleteByInvalidID() {
+    public void shouldValidateDeleteWhenDeleteByInvalidId() {
         ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
             EpicTask epicTask = createEpicTask();
             taskManager.addEpicTask(epicTask);
@@ -229,13 +230,130 @@ abstract class TaskManagerTest <T extends TaskManager> {
         });
         Assertions.assertEquals("Такого id нет", exception.getMessage());
     }
+
+    @Test
+    @DisplayName("Удаление по Id из пустого списка")
+    public void shouldValidateDeleteWhenDeleteFromEmptyList() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            int invalidId = 100;
+            taskManager.deleteById(invalidId);
+        });
+        Assertions.assertEquals("Такого id нет", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Редактирование задачи")
+    public void shouldUpdateSingleTask() {
+        SingleTask singleTask = createSingleTask();
+        taskManager.addSingleTask(singleTask);
+        singleTask.setDuration(707568400L);
+        singleTask.setStartTime(Instant.ofEpochMilli(9162714000000L));
+        singleTask.setTaskStatus(TaskStatus.IN_PROGRESS);
+        String expectedResult = "[" + singleTask + "]";
+        List<Task> tasks = taskManager.getAllSingleTasks();
+        assertEquals(expectedResult, tasks.toString());
+    }
+
+    @Test
+    @DisplayName("Редактирование эпика")
+    public void shouldUpdateEpicTaskWithSubtack() {
+        EpicTask epicTask = createEpicTask();
+        taskManager.addEpicTask(epicTask);
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addNewSubTask(subtask);
+        String expectedResult = "[" + epicTask + "]";
+        List<Task> tasks = taskManager.getAllEpicTasks();
+        assertEquals(expectedResult, tasks.toString());
+    }
+
+    @Test
+    @DisplayName("Редактирование подзадачи")
+    public void shouldUpdateSubtask() {
+        EpicTask epicTask = createEpicTask();
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addEpicTask(epicTask);
+        taskManager.addNewSubTask(subtask);
+        subtask.setDuration(707568400L);
+        subtask.setStartTime(Instant.ofEpochMilli(9162714000000L));
+        subtask.setTaskStatus(TaskStatus.IN_PROGRESS);
+        String expectedResult = "[" + subtask + "]";
+        List<Task> tasks = taskManager.getAllSubtasks();
+        assertEquals(expectedResult, tasks.toString());
+    }
+
+    @Test
+    @DisplayName("Редактирование Null задачи")
+    public void shouldNotUpdateSingleTaskIfNull() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            SingleTask singleTask = createSingleTask();
+            taskManager.addSingleTask(singleTask);
+            taskManager.updateSingleTask(null);
+        });
+        Assertions.assertEquals("Такой задачи нет", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Редактирование Null эпика")
+    public void shouldNotUpdateEpicIfNull() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            EpicTask epicTask = createEpicTask();
+            taskManager.addEpicTask(epicTask);
+            taskManager.updateEpicTask(null);
+        });
+        Assertions.assertEquals("Такой задачи нет", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Редактирование Null подзадачи")
+    public void shouldNotUpdateSubtaskIfNull() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            EpicTask epicTask = createEpicTask();
+            Subtask subtask = createSubtask(epicTask);
+            taskManager.addEpicTask(epicTask);
+            taskManager.addNewSubTask(subtask);
+            taskManager.updateSubtask(null);
+        });
+        Assertions.assertEquals("Такой задачи нет", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Редактирование задачи в Null листе")
+    public void shouldNotUpdateSingleTaskIfListIsNull() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            SingleTask singleTask = createSingleTask();
+            taskManager.updateSingleTask(singleTask);
+        });
+        Assertions.assertEquals("Такой задачи нет", exception.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("Редактирование эпика в Null листе")
+    public void shouldNotUpdateEpicIfListIsNull() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            EpicTask epicTask = createEpicTask();
+            taskManager.updateEpicTask(epicTask);
+        });
+        Assertions.assertEquals("Такой задачи нет", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Редактирование подзадачи в Null листе")
+    public void shouldNotUpdateSubtaskIfListIsNull() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            EpicTask epicTask = createEpicTask();
+            Subtask subtask = createSubtask(epicTask);
+            taskManager.addEpicTask(epicTask);
+            taskManager.updateSubtask(subtask);
+        });
+        Assertions.assertEquals("Такой задачи нет", exception.getMessage());
+    }
+
 }
 
 /*
-    void deleteById(int id);
-    void updateSingleTask(SingleTask singleTask);
-    void updateEpicTask(EpicTask epicTask);
-    void updateSubtask(Subtask subtask);
+
+
     List<Task> getAllSingleTasks();
     List<Task> getAllEpicTasks();
     List<Task> getAllSubtasks();
