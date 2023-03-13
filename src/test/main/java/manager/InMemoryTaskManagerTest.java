2c99a3d1f04ed5ed2fa150f10815154937cbfd8a
olganow <olganow@yandex.ru>
2023-03-13 17:21:23 +0300

    fix:  TaskManagerTest and InMemoryTaskManagerTest

--- src/test/main/java/manager/InMemoryTaskManagerTest.java
+++ src/test/main/java/manager/InMemoryTaskManagerTest.java
@@ -1,5 +1,7 @@
 package main.java.manager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
 
 
 class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
@@ -7,5 +9,8 @@ class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     public void beforeEach() {
         taskManager = new InMemoryTaskManager();
     }
-
+    @AfterEach
+    public void afterEach() {
+        taskManager.deleteAllTask();
+    }
 }
--- src/test/main/java/manager/TaskManagerTest.java
+++ src/test/main/java/manager/TaskManagerTest.java
@@ -4,7 +4,7 @@ import main.java.task.EpicTask;
 import main.java.task.SingleTask;
 import main.java.task.Subtask;
 import main.java.task.Task;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.Instant;
 import java.util.List;
@@ -14,163 +14,220 @@ import static org.junit.jupiter.api.Assertions.*;
 abstract class TaskManagerTest <T extends TaskManager> {
 
     protected T taskManager;
-    SingleTask singleTaskFirst;
-    private EpicTask epicTaskOne;
 
-    private Subtask subtaskOne;
+    protected EpicTask createEpicTask() {
 
+        return new EpicTask("First epic for testing", "Desc FE for testing ");
+    }
+    protected Subtask createSubtask(EpicTask epic) {
+        return new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW,
+                Instant.ofEpochMilli(163857900000L), 707568400L, epic.getId());
+    }
+
+
+
+    protected SingleTask createSingleTask() {
+        return new SingleTask( "First Single Task for testing", "Desc SST", TaskStatus.NEW,
+                Instant.ofEpochMilli(163857900000L), 57900000L);
+    }
 
 
     @Test
+    @DisplayName("Создание задачи")
     public void shouldCreateSingleTask() {
-        singleTaskFirst = new SingleTask("First Single Task for testing", "Desc SST", TaskStatus.NEW, Instant.ofEpochMilli(163857900000L), 707568400L);
-        taskManager.addSingleTask(singleTaskFirst);
+        SingleTask singleTask = createSingleTask();
+        taskManager.addSingleTask(singleTask);
+        String expectedResult = "[" + singleTask.toString() + "]";
         List<Task> tasks = taskManager.getAllSingleTasks();
-        assertEquals(List.of(singleTaskFirst), tasks);
+        assertEquals(expectedResult, tasks.toString());
     }
 
     @Test
+    @DisplayName("Создание эпика")
     public void shouldCreateEpicTask() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
+        EpicTask epicTask = createEpicTask();
+        taskManager.addEpicTask(epicTask);
+        String expectedResult = "[" + epicTask.toString() + "]";
         List<Task> tasks = taskManager.getAllEpicTasks();
-        assertEquals(List.of(epicTaskOne), tasks);
+        assertEquals(expectedResult, tasks.toString());
     }
 
     @Test
+    @DisplayName("Создание подзадачи")
     public void shouldCreateSubtask() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        subtaskOne = new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW, Instant.ofEpochMilli(163857900000L), 707568400L, 0);
-        taskManager.addNewSubTask(subtaskOne);
+        EpicTask epicTask = createEpicTask();
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addEpicTask(epicTask);
+        taskManager.addNewSubTask(subtask);
+        String expectedResult = "[" + subtask.toString() + "]";
         List<Task> tasks = taskManager.getAllSubtasks();
-        assertEquals(List.of(subtaskOne), tasks);
+        assertEquals(expectedResult, tasks.toString());
     }
 
     @Test
+    @DisplayName("Создание Null задачи")
     void shouldReturnNullWhenCreateSingleTaskNull() {
         SingleTask singleTask = null;
         taskManager.addSingleTask(singleTask);
-        assertNull(singleTask);
+        List<Task> tasks = taskManager.getAllSingleTasks();
+        assertNull(tasks);
     }
 
     @Test
+    @DisplayName("Создание Null эпика")
     void shouldReturnNullWhenCreateEpicTaskNull() {
         EpicTask epicTask = null;
         taskManager.addEpicTask(epicTask);
-        assertNull(epicTask);
+        List<Task> tasks = taskManager.getAllEpicTasks();
+        assertNull(tasks);
     }
 
     @Test
+    @DisplayName("Создание Null подзадачи")
     void shouldReturnNullWhenCreateSubtaskNull() {
         Subtask subtask = null;
         taskManager.addNewSubTask(subtask);
-        assertNull(subtask);
-    }
-
-    @Test
-    public void shouldAddValidIdWhenCreateSingleTaskWithInvalidId() {
-        int id = -1;
-        singleTaskFirst = new SingleTask(id, "First Single Task for testing", "Desc SST", TaskStatus.NEW,
-                Instant.ofEpochMilli(163857900000L), Instant.ofEpochMilli(1163857900000L));
-        taskManager.addSingleTask(singleTaskFirst);
-        List<Task> tasks = taskManager.getAllSingleTasks();
-        assertEquals(0, tasks.get(0).getId());
-    }
-
-    @Test
-    public void shouldAddValidIdWhenCreateEpicTaskWithInvalidId() {
-        int id = -1;
-        epicTaskOne = new EpicTask(id, "First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        List<Task> tasks = taskManager.getAllEpicTasks();
-        assertEquals(0, tasks.get(0).getId());
-    }
-
-    @Test
-    public void shouldAddValidIdWhenCreateSubtaskWithInvalidId() {
-        int id = -1;
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        subtaskOne = new Subtask(id, "First subtask for testing", "Desc FSB", TaskStatus.NEW, Instant.ofEpochMilli(163857900000L), Instant.ofEpochMilli(9163857900000L), 0);
-        taskManager.addNewSubTask(subtaskOne);
         List<Task> tasks = taskManager.getAllSubtasks();
-        assertEquals(1, tasks.get(0).getId());
+        assertNull(tasks);
+    }
+
+
+    @Test
+    @DisplayName("Создание задачи c невалидным id")
+    public void shouldAddValidIdWhenCreateSingleTaskWithInvalidId() {
+        int id = -100;
+        SingleTask singleTaskInvalid = new SingleTask(id, "First Single Task for testing", "Desc SST", TaskStatus.NEW,
+                Instant.ofEpochMilli(163857900000L), Instant.ofEpochMilli(1163857900000L));
+        taskManager.addSingleTask(singleTaskInvalid);
+        int expectedResult = 0;
+        assertEquals(expectedResult, singleTaskInvalid.getId());
     }
 
     @Test
+    @DisplayName("Создание эпика c невалидным id")
+    public void shouldAddValidIdWhenCreateEpicTaskWithInvalidId() {
+        int id = -100;
+        EpicTask epicTaskInvalid = new EpicTask(id, "First epic for testing", "Desc FE for testing ");
+        taskManager.addEpicTask(epicTaskInvalid);
+        int expectedResult = 0;
+        assertEquals(expectedResult, epicTaskInvalid.getId());
+    }
+
+    @Test
+    @DisplayName("Создание подзадачи c невалидным id")
+    public void shouldAddValidIdWhenCreateSubtaskWithInvalidId() {
+        int id = -100;
+        EpicTask epicTask = createEpicTask();
+        taskManager.addEpicTask(epicTask);
+        Subtask subtaskInvalid = new Subtask(id, "First subtask for testing", "Desc FSB", TaskStatus.NEW,
+                Instant.ofEpochMilli(163857900000L), Instant.ofEpochMilli(9163857900000L), epicTask.getId());
+        taskManager.addNewSubTask(subtaskInvalid);
+        int expectedResult = 1;
+        assertEquals(expectedResult, subtaskInvalid.getId());
+    }
+
+
+    @Test
+    @DisplayName("Удаление всех задач")
     public void shouldDeleteALLSingleTask() {
-        singleTaskFirst = new SingleTask("First Single Task for testing", "Desc SST", TaskStatus.NEW, Instant.ofEpochMilli(163857900000L), 707568400L);
-        taskManager.addSingleTask(singleTaskFirst);
+        SingleTask singleTask = createSingleTask();
+        taskManager.addSingleTask(singleTask);
         taskManager.deleteAllSingleTask();
         assertNull(taskManager.getAllSingleTasks());
 
     }
 
     @Test
+    @DisplayName("Удаление всех эпик без подзадач")
     public void shouldDeleteALLEpicTask() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
+        EpicTask epicTask = createEpicTask();
+        taskManager.addEpicTask(epicTask);
         taskManager.deleteAllEpicTask();
         assertNull(taskManager.getAllEpicTasks());
     }
 
     @Test
+    @DisplayName("Удаление всех эпиков с подзадачами")
     public void shouldDeleteALLEpicTaskWithAllSubtasks() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
+        EpicTask epicTask = createEpicTask();
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addEpicTask(epicTask);
+        taskManager.addNewSubTask(subtask);
         taskManager.deleteAllEpicTask();
         assertNull(taskManager.getAllSubtasks());
     }
 
     @Test
+    @DisplayName("Удаление всех подзадач")
     public void shouldDeleteALLSubtask() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        subtaskOne = new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW, Instant.ofEpochMilli(163857900000L), 707568400L, 0);
-        taskManager.addNewSubTask(subtaskOne);
+        EpicTask epicTask = createEpicTask();
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addEpicTask(epicTask);
+        taskManager.addNewSubTask(subtask);
         taskManager.deleteAllEpicTask();
         assertNull(taskManager.getAllSubtasks());
     }
 
+
     @Test
+    @DisplayName("Удаление задачи по ID")
     public void shouldDeleteSingleTaskById() {
-        singleTaskFirst = new SingleTask("First Single Task for testing", "Desc SST", TaskStatus.NEW,
-                Instant.ofEpochMilli(163857900000L), 7900000L);
-        taskManager.addSingleTask(singleTaskFirst);
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        taskManager.deleteById(0);
+        SingleTask singleTask = createSingleTask();
+        taskManager.addSingleTask(singleTask);
+        EpicTask epicTask = createEpicTask();
+
+        taskManager.addEpicTask(epicTask);
+        taskManager.deleteById(singleTask.getId());
         assertNull(taskManager.getAllSingleTasks());
     }
 
     @Test
+    @DisplayName("Удаление эпика без сабтасок по ID")
     public void shouldDeleteEpicTaskById() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        taskManager.deleteById(0);
+        SingleTask singleTask = createSingleTask();
+        EpicTask epicTask = createEpicTask();
+        taskManager.addSingleTask(singleTask);
+        taskManager.addEpicTask(epicTask);
+        taskManager.deleteById(epicTask.getId());
         assertNull(taskManager.getAllEpicTasks());
     }
 
     @Test
+    @DisplayName("Удаление эпика с сабтасками по ID")
     public void shouldDeleteEpicTaskWithSubtaskById() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        subtaskOne = new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW, Instant.ofEpochMilli(163857900000L), 163857900000L, 0);
-        taskManager.addNewSubTask(subtaskOne);
-        taskManager.deleteById(0);
+        SingleTask singleTask = createSingleTask();
+        EpicTask epicTask = createEpicTask();
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addSingleTask(singleTask);
+        taskManager.addEpicTask(epicTask);
+        taskManager.addNewSubTask(subtask);
+        taskManager.deleteById(epicTask.getId());
         assertNull(taskManager.getAllSubtasks());
     }
 
     @Test
+    @DisplayName("Удаление сабтаски по ID")
     public void shouldDeleteSubtaskById() {
-        epicTaskOne = new EpicTask("First epic for testing", "Desc FE for testing ");
-        taskManager.addEpicTask(epicTaskOne);
-        subtaskOne = new Subtask("First subtask for testing", "Desc FSB", TaskStatus.NEW,
-                Instant.ofEpochMilli(163857900000L), 9163857900000L, 0);
-        taskManager.addNewSubTask(subtaskOne);
-        taskManager.deleteById(1);
+        EpicTask epicTask = createEpicTask();
+        taskManager.addEpicTask(epicTask);
+        Subtask subtask = createSubtask(epicTask);
+        taskManager.addNewSubTask(subtask);
+        taskManager.deleteById(subtask.getId());
         assertNull(taskManager.getAllSubtasks());
+    }
+
+    @Test
+    @DisplayName("Удаление с некорректным ID")
+    public void shouldValidateDeleteWhenDeleteByInvalidID() {
+        ManagerSaveException exception = Assertions.assertThrows(ManagerSaveException.class, () -> {
+            EpicTask epicTask = createEpicTask();
+            taskManager.addEpicTask(epicTask);
+            Subtask subtask = createSubtask(epicTask);
+            taskManager.addNewSubTask(subtask);
+            int invalidId = 100;
+            taskManager.deleteById(invalidId);
+        });
+        Assertions.assertEquals("Такого id нет", exception.getMessage());
     }
 }
 
